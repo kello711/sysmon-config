fbc5999f4d86eb9691d92424b05d48bdce08ae1b
kello711 <kello711@gmail.com>
2017-11-21 12:22:00 -0600

    Updated configs

--- sysmon-deploy.ps1
+++ sysmon-deploy.ps1
@@ -1,14 +1,23 @@
-$computers = Get-Content "C:\computer.txt"
+Set-Item wsman:\localhost\client\trustedhosts * -Force
+$cred = Get-Credential username
+$computers = Get-Content "\path\to\computer.txt"
 
-foreach ($computer in $computers) {
-    #The location of the file
-    $Install = "\\$computer\C$\Software"
+foreach($computer in $computers) {
+    Copy-Item -Path "\path\to\filename.zip" -Destination "\\$computer\C$\afs-agent.zip" -Force
 
-    #The Install string can have commands aswell
-    $InstallString = "$Install\yourinstaller.exe"
+    Invoke-Commmand -ComputerName $computer -Credential $cred -ScriptBlock {
+        Expand-Archive "C:\afs-agent.zip" -DestinationPath "C:\afs-agent" -Force
+    }
 
-    ([WMICLASS]"\\$computer\ROOT\CIMV2:Win32_Process").Create($InstallString)
+    Remove-Item -Path "\\$computer\C$\afs-agent.zip" -Force
 
-    #Output the install result to your Local C Drive
-    Out-File -FilePath c:\installed.txt -Append -InputObject "$computer"
+    Invoke-Command -ComputerName $computer -Credential $cred -ScriptBlock {
+        Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+        C:\afs-agent\sysmon.exe -accepteula -i C:\afs-agent\sysmon-config.xml
+        C:\afs-agent\winlogbeat\install-service-winlogbeat.ps1
+        Start-Service winlogbeat
+        Get-Service winlogbeat
+    }
+
+    #Out-File -FilePath "C:\installed.txt" -Append -InputObject "$computer"
 }
